e02480452c39cbf3df9cd57b53d28bebe879731e
Antony Bartolomucci <antony@passbolt.com>
2024-08-27 10:59:59 +0200

    Add warning for users usage

--- src/cli/main.js
+++ src/cli/main.js
@@ -1,9 +1,16 @@
-import { getPrivateKeyAndPassphrase, getUserFileInput } from './views/cliPrompts.js';
+import {getPrivateKeyAndPassphrase, getUserFileInput, Warning} from './views/cliPrompts.js';
 import { decryptUserFile } from './services/decryptionService.js';
 import { handleCli } from './controllers/passwordController.js';
 
 async function startCli() {
     try {
+        const { acknowledge } = await Warning();
+
+        if (!acknowledge) {
+            console.log('Exiting for security reasons.');
+            return;
+        }
+
         const { privateKeyFile, passphrase } = await getPrivateKeyAndPassphrase();
 
         const { userFile } = await getUserFileInput();
